f9c027a3810878fc4e029e36a0efe974a7d0d25c
AndrewSimons3 <simdrew07@gmail.com>
2022-08-04 08:38:52 -0700

    finished card

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const shareBtn = document.getElementById('share-btn');
+const active = document.getElementById('active');
+const shareBtn2 = document.getElementById('share-btn-2');
+
+console.log('hello world');
+
+shareBtn.addEventListener('click', () => {
+  active.classList.toggle('active-state')
+});
+
+shareBtn2.addEventListener('click', () => {
+	active.classList.toggle('active-state');
+});
+
